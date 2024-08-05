bdb58b6abb250622b34d25c1da794dcefae4ae38
Gitsalov Andrey <gicalov.fortech@gmail.com>
2024-08-05 21:26:05 +0300

    fix: code-style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
-
 import React, { useState, useRef, useEffect } from "react";
-import { HexColorPicker } from "react-colorful";
-import { debounce } from "lodash";
+import CustomCanvas from "../components/CustomCanvas";
+import CanvasToolbar from "../components/CanvasToolbar";
+import { StyledContainer } from "./styles";
 
-interface DrawingCanvasProps {}
-
-const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
+const DrawingCanvas: React.FC = () => {
   const [color, setColor] = useState<string>("#000000");
   const [brushSize, setBrushSize] = useState<number>(5);
-  const [isDrawing, setIsDrawing] = useState<boolean>(false);
   const [isErasing, setIsErasing] = useState(false);
-  const [divSize, setDivSize] = useState({ width: 30, height: 30 });
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
-  const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -31,46 +26,7 @@ const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
     }
   }, [color, brushSize]);
 
-  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    const { offsetX, offsetY } = e.nativeEvent;
-
-    if (contextRef.current) {
-      contextRef.current.beginPath();
-      contextRef.current.moveTo(offsetX, offsetY);
-      contextRef.current.lineWidth = brushSize;
-      if (isErasing) {
-        contextRef.current.strokeStyle = "white";
-      } else {
-        contextRef.current.strokeStyle = color;
-      }
-      setIsDrawing(true);
-    }
-  };
-
-  const saveCanvasToLocalStorage = () => {
-    const canvasData = canvasRef.current!.toDataURL();
-
-    localStorage.setItem("canvasData", canvasData);
-  };
-
-  const finishDrawing = () => {
-    if (contextRef.current) {
-      contextRef.current.closePath();
-      setIsDrawing(false);
-
-      saveCanvasToLocalStorage();
-    }
-  };
-
-  const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!isDrawing || !contextRef.current) return;
-
-    const { offsetX, offsetY } = e.nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
-  };
-
-  const clearCanvas = () => {
+  const handleClearCanvas = () => {
     const canvas = canvasRef.current;
 
     if (canvas) {
@@ -93,81 +49,29 @@ const DrawingCanvas: React.FC<DrawingCanvasProps> = () => {
     setBrushSize(Number(event.target.value));
   };
 
-  const loadCanvasFromLocalStorage = () => {
-    const canvasData = localStorage.getItem("canvasData");
-
-    if (canvasData) {
-      const img = new Image();
-      img.src = canvasData;
-      const context = canvasRef.current!.getContext("2d");
-      img.onload = () => {
-        context!.drawImage(img, 0, 0);
-      };
-      console.log(context);
-    }
+  const handleChangeMode = () => {
+    setIsErasing((prevState) => !prevState);
   };
 
-  useEffect(() => {
-    const handleResize = debounce(() => {
-      if (divRef.current) {
-        setDivSize({
-          width: window.innerWidth - 200,
-          height: divRef.current.offsetHeight,
-        });
-      }
-      loadCanvasFromLocalStorage();
-    }, 200);
-    handleResize();
-    loadCanvasFromLocalStorage();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-      }}
-    >
-      <div ref={divRef} style={{ width: "100%", height: "100%" }}>
-        <canvas
-          ref={canvasRef}
-          width={divSize.width}
-          height={divSize.height}
-          style={{
-            border: "1px solid #000",
-          }}
-          onMouseDown={startDrawing}
-          onMouseUp={finishDrawing}
-          onMouseMove={draw}
-          onMouseLeave={() => setIsDrawing(false)}
-        />
-      </div>
-      <div>
-        <label htmlFor="color">Цвет кисти:</label>
-        <HexColorPicker color={color} onChange={handleColorChange} />
-        <label htmlFor="brushSize">Толщина кисти:</label>
-        <input
-          type="range"
-          id="brushSize"
-          min="1"
-          max="50"
-          value={brushSize}
-          onChange={handleBrushSizeChange}
-        />
-        <button onClick={clearCanvas}>Очистить холст</button>
-        <button onClick={() => setIsErasing((prevState) => !prevState)}>
-          {isErasing ? "ластик" : "кисть"}
-        </button>
-      </div>
-    </div>
+    <StyledContainer>
+      <CustomCanvas
+        canvasRef={canvasRef}
+        contextRef={contextRef}
+        isErasing={isErasing}
+        brushSize={brushSize}
+        color={color}
+      />
+      <CanvasToolbar
+        color={color}
+        handleColorChange={handleColorChange}
+        brushSize={brushSize}
+        handleBrushSizeChange={handleBrushSizeChange}
+        handleClearCanvas={handleClearCanvas}
+        isErasing={isErasing}
+        handleChangeMode={handleChangeMode}
+      />
+    </StyledContainer>
   );
 };
 
---
+++ src/app/styles.ts
@@ -0,0 +1,13 @@
+import style from "styled-components";
+
+export const StyledContainer = style.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const StyledDiv = style.div`
+width: 100%; height: 100%;
+border: 1px solid #000,
+`;
